56404aba30e34dd92424f7ffcd715bbb59eba947
Manuel della Gala <manuel.dellagala@gmail.com>
2021-04-17 14:51:27 +0200

    correggere map

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function main(){
+
+    //snack 1
     var biciDaCorsa = [
     {
     nome:'uno',
@@ -9,17 +11,19 @@ function main(){
     peso:10,
   }
   ];
-  var x=biciDaCorsa[0];
+  var vA=biciDaCorsa[0];
   
   for (k in biciDaCorsa){
     console.log(biciDaCorsa[k]);
-    x = (x.peso>(biciDaCorsa[k].peso))? //se x > bici peso 
+    vA = (vA.peso>(biciDaCorsa[k].peso))? //se x > bici peso 
     biciDaCorsa[k] // allora restituisco il peso della bici corrente
-    :x;   // altrimenti x rimane lo stesso
+    :vA;   // altrimenti x rimane lo stesso
   }
   
-  console.log(`La bici più leggera è la ${x.nome} che pesa ${x.peso} Kg`);
+  console.log(`La bici più leggera è la ${vA.nome} che pesa ${vA.peso} Kg`);
 
+
+// snack 2
   var squadre = [
     {
         nome:'uno',
@@ -69,10 +73,77 @@ var newObj=[];
 
     console.table(squadre);
     console.table(newObj);
+    //snack 3
+
+    //var lista=[2,6,7,4,8,9,43,10,5,3];
+    var lista = [];
+    for(var i=0 ; i<10;i++)
+        lista[i]=rnd(1,10);
+    console.log(lista);
+    var max=0,min=0;
+    while (((max-min)<=0)||(isNaN(max)||(isNaN(min)))){ // controllo che max è maggiore di min e che siano entrambi numeri
+        min=Math.abs(parseInt(prompt('inserisci il valore minimo'))); // ritorno un valore intero e positivo
+        max=Math.abs(parseInt(prompt('inserici il valore massimo')));   // ritorno un valore intero e positivo
+        //console.log(min,max);   // vedo i valori immessi 
+    }
+
+    console.log('for',subString(lista,min,max)); // stampo l'array
+
+    
+    console.log('filter',subStringFilter(lista,min,max));
+
+    var map = subStringMap(lista,min,max); 
+    console.log('map',map);
   
+    
   
 
 }
+// snack 3 funzione con for
+    function subString(arr,a,b){
+     var min = Math.abs(b-a);  // gioco con i valori potrei inserire i valori in un ordine qualsiasi
+     var max = (a<b) ? b : a;
+     var arrTmp = [];
+     for (i in arr)
+        if(i<=(max)&& i>=(max-min))
+            arrTmp.push(arr[i]);
+        return arrTmp;
+    }
+//funzione con filter
+    function subStringFilter(arr,a,b){
+         
+        return arr.filter((i)=>{
+            //console.log(val,i); 
+            return  (i>=a&&i<=b);
+            });
+            
+        
+           
+       }
+
+    //funzione con map
+    function subStringMap(arr,a,b){
+         
+        return arr.map((val,i)=>{
+            tmp = (i>=a&&i<=b)? val.i : 0;
+            return  tmp;
+            });
+            
+        
+           
+       }
+
+      /* var numbers = [65, 44, 12, 4];
+var newarray = numbers.map(myFunction)
+
+function myFunction(num) {
+  return num * 10;
+}*/
+
+document.getElementById("demo").innerHTML = newarray;
+
+
+
 
 function rnd(max,min){
     return Math.floor(Math.random()*(max-min+1)+min);
--- readme.md
+++ readme.md
@@ -9,3 +9,13 @@ Nome sarà l'unica proprietà da compilare, le altre saranno tutte settate a 0.
 Generare numeri random al posto degli 0 nelle proprietà:
 Punti fatti e falli subiti
 Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+jsnack 3
+Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+inseriti dall'utente (è possibile usare, ad esempio, for/foreach/filter
+
+jsnack 4
+Dato un'array con dei capi d'abbigliamento - oggetti che contengono informazioni su nome modello, tipologia e colore - si aggiunga a ciascun elemento una ulteriore proprietà che indichi il costo del prodotto.
+Per inserire il costo del singolo prodotto si scriva una funzione che generi un numero random da 10 a 50 (potete sfruttare il map per aggiungere la nuova proprietà)
